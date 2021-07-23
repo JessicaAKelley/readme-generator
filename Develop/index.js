cf6a3041d8bd8e513e948ffef4861128a0651019
JessicaAKelley <kelleyajessica@gmail.com>
2021-07-23 13:42:10 -0500

    Question prompts for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,80 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Question 1: Project Title
+    {
+        name: "title",
+        type: "input",
+        message: "What is your project title?",
+    },
+    // Question 2: Description of Project
+    {
+        name: "description",
+        type: "input",
+        message: "Please describe your project.",
+    },
+    // Question 3: Table of Contents
+
+    // Question 4: Installation
+    {
+        name: "installation",
+        type: "input",
+        message: "Please describe the set-up/installation process for your project.",
+    },
+    // Question 5: Usage
+    {
+        name: "usage",
+        type: "input",
+        message: "Please provide instructions and examples for use.",
+    },
+    // Question 6: Contribution
+    {
+        name: "contribution",
+        type: "input",
+        message: "Please provide any and all project contributors.",
+    },
+    // Question 7: License
+    {
+        name: "license",
+        type: "list",
+        message: "Please choose from the below license options.",
+        options: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Mozilla Public License 2.0",
+            "Apache License 2.0",
+            "MIT License",
+            "Boost Software License 1.0",
+            "The Unlicense",
+        ],
+    },
+    // Question 8: Tests
+    {
+        name: "tests",
+        type: "input",
+        message: "Please describe how to run any tests.",
+    },
+    // Question 9: Questions Contact Github
+    {
+        name: "questions",
+        type: "input",
+        message: "Please provide your Github username.",
+    },
+    // Question 10: Questions Contact Email
+    {
+        name: "questions",
+        type: "input",
+        message: "Please provide your email address.",
+    },
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
