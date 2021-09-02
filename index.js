c78495ffdf30a7878237e75398091be3f28d42c9
JessicaAKelley <kelleyajessica@gmail.com>
2021-09-02 11:29:59 -0500

    Markdown and user input edits. Appears to be working.

--- index.js
+++ index.js
@@ -1,92 +1,187 @@
-// TODO: Include packages needed for this application
+// TO DO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./Develop/utils/generateMarkdown");
+const generateMarkdown = require("./Develop/utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
+// TO DO: Create an array of questions for user input
 const questions = [
-    // Question 1: Project Title
-    {
-        name: "title",
-        type: "input",
-        message: "What is your project title?",
-    },
-    // Question 2: Description of Project
-    {
-        name: "description",
-        type: "input",
-        message: "Please describe your project.",
-    },
-    // Question 3: Table of Contents
+  // Question 1: Developer Name
+  "What is your name? (Required)",
 
-    // Question 4: Installation
-    {
-        name: "installation",
-        type: "input",
-        message: "Please describe the set-up/installation process for your project.",
-    },
-    // Question 5: Usage
-    {
-        name: "usage",
-        type: "input",
-        message: "Please provide instructions and examples for use.",
-    },
-    // Question 6: Contribution
-    {
-        name: "contribution",
-        type: "input",
-        message: "Please provide any and all project contributors.",
-    },
-    // Question 7: License
-    {
-        name: "license",
-        type: "list",
-        message: "Please choose from the below license options.",
-        choices: [
-            "GNU AGPLv3",
-            "GNU GPLv3",
-            "GNU LGPLv3",
-            "Mozilla Public License 2.0",
-            "Apache License 2.0",
-            "MIT License",
-            "Boost Software License 1.0",
-            "The Unlicense",
-        ],
-    },
-    // Question 8: Tests
-    {
-        name: "tests",
-        type: "input",
-        message: "Please describe how to run any tests.",
-    },
-    // Question 9: Questions Contact - Github
-    {
-        name: "github",
-        type: "input",
-        message: "Please provide your Github username.",
-    },
-    // Question 10: Questions Contact - Email
-    {
-        name: "email",
-        type: "input",
-        message: "Please provide your email address.",
-    },
+  // Question 2: Developer GitHub Username
+  "What is your GitHub Username? (Required)",
 
+  // Question 3: Developer Email
+  "What is your email? (Required)",
+
+  // Question 4: Project Title
+  "What is your project title? (Required)",
+
+  // Question 5: Project Description
+  "Please provide a description of your project. (Required)",
+
+  // Question 6: Installation
+  "Please describe the set-up/installation process for your project.",
+
+  // Question 7: Usage
+  "Please provide instructions and examples for project use.",
+
+  // Question 8: Contribution
+  "Please provide any and all project contributors.",
+
+  // Question 9: Tests
+  "Please describe how to run any tests.",
+
+  // Question 10: License
+  "Which license is the application covered under?",
 ];
 
-// TODO: Create a function to write README file
+// TO DO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function (err) {
-        if (err) throw error;
-        console.log("The README file has been successfully generated!");
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileName, data, (err) => {
+      // If an error occurs, reject Promise and send error to Promise's `.catch()` method
+      if (err) {
+        reject(err);
+        // Return out of function
+        return;
+      }
+
+      // If successful, resolve Promise and send successful data to `.then()` method
+      resolve({
+        ok: true,
+        message: "README has been successfully generated!",
+      });
     });
+  });
 }
 
-// TODO: Create a function to initialize app
+// TO DO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(function (userInput) {
-        writeToFile("README.md", generateMarkdown(userInput));
-    });
+  const [
+    nameQ,
+    githubQ,
+    emailQ,
+    titleQ,
+    descriptionQ,
+    installationQ,
+    usageQ,
+    contributionQ,
+    testQ,
+    licenseQ,
+  ] = questions;
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: nameQ,
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: githubQ,
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: emailQ,
+      validate: (emailInput) => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("Please enter your email address.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "title",
+      message: titleQ,
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter your project title.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: descriptionQ,
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("Please provide a description of your project.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: installationQ,
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: usageQ,
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: contributionQ,
+    },
+    {
+      type: "input",
+      name: "test",
+      message: testQ,
+    },
+    {
+      type: "list",
+      name: "license",
+      message: licenseQ,
+      choices: [
+        "GNU AGPLv3",
+        "GNU GPLv3",
+        "GNU LGPLv3",
+        "Mozilla Public License 2.0",
+        "Apache License 2.0",
+        "MIT License",
+        "Boost Software License 1.0",
+        "The Unlicense",
+      ],
+    },
+  ]);
+  // .then((answers) => {
+  //   writeToFile("README.md", generateMarkdown(answers));
+  // })
+  // .catch((error) => {
+  //   if (error.isTtyError) {
+  //     console.log("Prompt couldn't be rendered in the current environment");
+  //   } else {
+  //     console.log("Something else went wrong");
+  //   }
+  // });
 }
 
 // Function call to initialize app
